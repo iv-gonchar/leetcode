c88324a3dec5febc73bb226614b41ccb6b006c56
iv.gonchar <gonchar.ivan@gmail.com>
2020-07-31 16:53:32 +0300

    add unit-tests for MaxConsecutiveOnes problem

--- src/test/java/org/gonchar/leetcode.arrays101/MaxConsecutiveOnesTest.java
+++ src/test/java/org/gonchar/leetcode.arrays101/MaxConsecutiveOnesTest.java
@@ -1,7 +1,48 @@
 package org.gonchar.leetcode.arrays101;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MaxConsecutiveOnesTest {
 
+    private MaxConsecutiveOnes solution = new MaxConsecutiveOnes();
+
+    @Test
+    void nullInput() {
+        Exception exception = assertThrows(NullPointerException.class, () -> solution.findMaxConsecutiveOnes(null));
+        assertEquals("Input should not be null", exception.getMessage());
+    }
+
+    @Test
+    void emptyArrayInput() {
+        assertEquals(0, solution.findMaxConsecutiveOnes(new int[]{}));
+    }
+
+    @Test
+    void noOnesInInput() {
+        assertEquals(0, solution.findMaxConsecutiveOnes(new int[]{0, 0, 0, 0}));
+    }
+
+    @Test
+    void onesInTheBeginning() {
+        assertEquals(3, solution.findMaxConsecutiveOnes(new int[]{1, 1, 1, 0, 0, 0, 0}));
+    }
+
+    @Test
+    void onesInTheMiddle() {
+        assertEquals(2, solution.findMaxConsecutiveOnes(new int[]{0, 0, 0, 0, 1, 1, 0, 0}));
+    }
+
+    @Test
+    void onesInTheEnd() {
+        assertEquals(4, solution.findMaxConsecutiveOnes(new int[]{0, 0, 0, 0, 0, 1, 1, 1, 1}));
+    }
+
+    @Test
+    void severalSeries() {
+        assertEquals(5, solution.findMaxConsecutiveOnes(
+                new int[]{0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0}));
+    }
 }
