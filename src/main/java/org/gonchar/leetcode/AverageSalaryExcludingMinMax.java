2d0ef4d538fbc6513b88c675a78dbdcc19970f9e
iv.gonchar <gonchar.ivan@gmail.com>
2021-02-20 18:51:40 +0200

    copy wrong solution from browser

---
+++ src/main/java/org/gonchar/leetcode/AverageSalaryExcludingMinMax.java
@@ -0,0 +1,22 @@
+package org.gonchar.leetcode;
+
+public class AverageSalaryExcludingMinMax {
+
+    public double average(int[] salary) {
+        double average = 0.0;
+        double min = 10000000.0; // greater than any salary according to constraint
+        double max = 10.0; // less than any salary according to constraint
+        for (int i=0; i<salary.length; i++) {
+            double current = salary[i] / (salary.length - 2);
+            if (max < current) {
+                max = current;
+            }
+            if (min > current) {
+                min = current;
+            }
+            average = average + current;
+        }
+        average = average - min - max;
+        return average;
+    }
+}
