897006535697632887877f01d1899d03822b75c1
iv.gonchar <gonchar.ivan@gmail.com>
2021-03-01 13:40:21 +0200

    copy my solution from browser

---
+++ src/main/java/org/gonchar/leetcode/strings/ReverseString.java
@@ -0,0 +1,16 @@
+package org.gonchar.leetcode.strings;
+
+public class ReverseString {
+
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+        while (l < r) {
+            char c = s[l];
+            s[l] = s[r];
+            s[r] = c;
+            l++;
+            r--;
+        }
+    }
+}
