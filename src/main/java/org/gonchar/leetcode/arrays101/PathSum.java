cf79b78273c9d8dfcc4d675bede7ac9586b7a1de
iv.gonchar <gonchar.ivan@gmail.com>
2021-04-06 22:11:02 +0300

    copy my solution from browser

---
+++ src/main/java/org/gonchar/leetcode/arrays101/PathSum.java
@@ -0,0 +1,33 @@
+package org.gonchar.leetcode.arrays101;
+
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        int newSum = targetSum - root.val;
+        return hasPathSum(root.left, newSum) || hasPathSum(root.right, newSum);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
