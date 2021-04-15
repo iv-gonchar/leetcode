6654dcc095a08c0b3048127f6ab600b7f9db4af5
iv.gonchar <gonchar.ivan@gmail.com>
2021-04-15 22:04:29 +0300

    copy dp solution

--- src/main/java/org/gonchar/leetcode/arrays101/Solution.java
+++ src/main/java/org/gonchar/leetcode/arrays101/Solution.java
@@ -4,27 +4,21 @@ import java.util.Arrays;
 
 public class Solution {
     public int lengthOfLIS(int[] nums) {
-        // [prev][curr]
-        int[][] memo = new int[nums.length+1][nums.length];
-        for(int i=0; i<memo.length; i++) {
-            Arrays.fill(memo[i], -1);
-        }
-        return maxLISLength(-1, 0, nums, memo);
-    }
+        int[] dp = new int[nums.length];
 
-    public int maxLISLength(int prev, int cur, int[] nums, int[][] memo) {
-        if (cur == nums.length) {
-            return 0;
+        int maxLength = 0;
+        for(int i=0; i<nums.length; i++) {
+            int max = 0;
+            for(int j=0; j<=i-1; j++) {
+                if (nums[i]>nums[j] && max<dp[j]) {
+                    max = dp[j];
+                }
+            }
+            dp[i] = max + 1;
+            if (dp[i] > maxLength) {
+                maxLength = dp[i];
+            }
         }
-        if (memo[prev+1][cur] >= 0) {
-            return memo[prev+1][cur];
-        }
-        int taken = 0;
-        if (prev == -1 || nums[cur] > nums[prev]) {
-            taken = 1 + maxLISLength(cur, cur + 1, nums, memo);
-        }
-        int notTaken = maxLISLength(prev, cur + 1, nums, memo);
-        memo[prev+1][cur] = Math.max(taken, notTaken);
-        return memo[prev+1][cur];
+        return maxLength;
     }
 }
