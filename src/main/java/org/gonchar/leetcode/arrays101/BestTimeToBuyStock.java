9d913be0fe075ba9442b51cbe7a98ac3a6d8e829
iv.gonchar <gonchar.ivan@gmail.com>
2021-04-16 20:48:06 +0300

    update with the best solution

--- src/main/java/org/gonchar/leetcode/arrays101/BestTimeToBuyStock.java
+++ src/main/java/org/gonchar/leetcode/arrays101/BestTimeToBuyStock.java
@@ -1,43 +1,20 @@
 package org.gonchar.leetcode.arrays101;
 
 public class BestTimeToBuyStock {
-
-    /*
-        This solution is not effective enough
-     */
     public int maxProfit(int[] prices) {
-        int[] firstIndex = new int[10001];
-        int[] lastIndex = new int[10001];
-        int min = 10001;
-        int max = 0;
-
-        for (int i = 0; i <= 10000; i++) {
-            firstIndex[i] = -1;
-            lastIndex[i] = -1;
-        }
-
-        for (int i = 0; i < prices.length; i++) {
-            int price = prices[i];
-            if (price > max) {
-                max = price;
+        // idea
+        int currentMin = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i=0; i<prices.length; i++) {
+            if (prices[i] < currentMin) {
+                currentMin = prices[i];
+                continue;
             }
-            if (price < min) {
-                min = price;
-            }
-            if (firstIndex[price] == -1) {
-                firstIndex[price] = i;
-            }
-            lastIndex[price] = i;
-        }
-
-        int maxProfit = max - min;
-        for (int profit = maxProfit; profit >= 0; profit--) {
-            for (int i = min; i+profit <= max; i++) {
-                if (firstIndex[i] != -1 && firstIndex[i] < lastIndex[i + profit]) {
-                    return profit;
-                }
+            int profit = prices[i] - currentMin;
+            if (profit > maxProfit) {
+                maxProfit = profit;
             }
         }
-        return 0;
+        return maxProfit;
     }
 }
